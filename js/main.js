7878a16830550ade0f04ce839be72f9eb36a21a3
Miguel del Rio <miguel.delrio@rev.com>
2022-06-01 19:28:29 -0400

    updating website with more recent work

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ var pages = {
 		'color':'#3498db',
 		'title':"About Me",
 		'subtitle':'Get to know a little bit about me and my interests.',
-		'main':"Welcome to my GitHub Pages! This website is meant to be a brief summary about me -- if you'd like to chat more please feel free to reach out to me via my email.<br/><br/>I graduated from my Master's of Engineering in Computer Science from MIT in January 2020, focusing in Artificial Intelligence / Machine Learning. In recent years, my interests have been in Natural Language Processing (NLP), Automatic Speech Recognition (ASR), and Computational Psycholinguistics.<br/><br/>Some interesting facts about me:<ul><li>My favorite authors are Dan Brown (I would highly recommend <i>Digital Fortress</i> or <i>Origins</i>) and Kurt Vonnegut (<i>Player Piano</i> is a must read)</li><li>I am bilingual, fluent in Spanish and Engilsh but want to learn German!</li><li>My favorite work of art is <i>Guernica</i> by Pablo Picasso.</li><ul>"
+		'main':"Welcome to my GitHub Page! This website is meant to be a brief summary about me -- if you'd like to chat more please feel free to reach out to me via my email.<br/><br/>I graduated from my Master's of Engineering in Electrical Engineering and Computer Science from MIT in February 2020, focusing in Artificial Intelligence / Machine Learning. In recent years, my interests have been in Natural Language Processing (NLP), Automatic Speech Recognition (ASR), and Computational Psycholinguistics.<br/><br/>Some interesting facts about me:<ul><li>My favorite authors are Dan Brown (I would highly recommend <i>Digital Fortress</i> or <i>Origins</i>) and Kurt Vonnegut (<i>Player Piano</i> is a must read)</li><li>I am bilingual, fluent in Spanish and English but want to learn German!</li><li>My favorite work of art is <i>Guernica</i> by Pablo Picasso.</li><ul>"
 	},
 	'Resume':{
 		'color':'#9b59b6',
@@ -14,37 +14,55 @@ var pages = {
 		'color':'#27ae60',
 		'title':"Publications",
 		'main':"\
-		<span style='text-align:center; font-weight:bold; font-size:1.25em;'>2021</span>\
+		<span style='text-align:center; font-weight:bold; font-size:1.5em;'>2022</span>\
 		<br/>\
-		Miguel Del Rio, Natalie Delworth, Ryan Westerman, Michelle Huang, Nishchal Bhandari, Joseph Palakapilly Quinten McNamara, Joshua Dong, Piotr <span>&#379;</span>elasko, Miguel Jette. \"Earnings 21: A Practical Benchmark for ASR in the Wild\". <a class='links' href='https://arxiv.org/abs/2104.11348'>https://arxiv.org/abs/2104.11348</a>.\
+		Miguel Del Rio, Peter Ha, Quinten McNamara, Corey Miller, Shipra Chandra. \"Earnings-22: A Practical Benchmark for Accents in the Wild\".\
 		<br/>\
-		<em>Accepted at Interspeech 2021</em>\
+		<em>Submitted to Interspeech 2022</em>\
 		<br/>\
-		<br/>\
-		Arthur Hinsvark, Natalie Delworth, Miguel Del Rio, Quinten McNamara, Joshua Dong, Ryan Westerman, Michelle Huang, Joseph Palakapilly, Jennifer Drexler, Ilya Pirkin, Nishchal Bhandari, Miguel Jette. \"Accented Speech Recognition: A Survey\". <a class='links' href='https://arxiv.org/abs/2104.10747'>https://arxiv.org/abs/2104.10747</a>.\
-		<br/>\
-		<br/>\
-		Matteo Alleman, Jonathan Mamou, Miguel A Del Rio, Hanlin Tang, Yoon Kim, SueYeon Chung. \"Syntactic Perturbations Reveal Representational Correlates of Hierarchical Phrase Structure in Pretrained Language Models\". In Proceedings of the 6th Workshop on Representation Learning for NLP (RepL4NLP-2021). <a class='links' href='https://aclanthology.org/2021.repl4nlp-1.27.pdf'>https://aclanthology.org/2021.repl4nlp-1.27.pdf</a>.\
+		<a class='links' href='https://arxiv.org/abs/2203.15591'>ArXiv Link</a>\
 		<br/>\
 		<br/>\
-		<span style='text-align:center; font-weight:bold; font-size:1.25em;'>2020</span>\
+		<span style='text-align:center; font-weight:bold; font-size:1.5em;'>2021</span>\
 		<br/>\
-		Jonathan Mamou, Hang Le, Miguel Del Rio, Cory Stephenson, Hanlin Tang, Yoon Kim, and SueYeon Chung. \"Emergence of separable manifolds in deep language representations\". In 37<sup>th</sup> International Conference on Machine Learning, ICML 2020. International Machine LearningSociety (IMLS), 2020. <a class='links' href='https://arxiv.org/abs/2006.01095'>https://arxiv.org/abs/2006.01095</a>."
+		Del Rio, M., Delworth, N., Westerman, R., Huang, M., Bhandari, N., Palakapilly, J., McNamara, Q., Dong, J., &Zdot;elasko, P., Jett&eacute;, M. (2021) Earnings-21: A Practical Benchmark for ASR in the Wild. Proc. Interspeech 2021, 3465-3469, doi: 10.21437/Interspeech.2021-1915.\
+		<br/>\
+		<a class='links' href='https://arxiv.org/abs/2104.11348'>ArXiv Link</a> - <a href=\"https://www.superlectures.com/interspeech2021/earnings-21-a-practical-benchmark-for-asr-in-the-wild-3-minutes-introduction\">Video Presentation</a> - <a href=\"https://www.isca-speech.org/archive/interspeech_2021/delrio21_interspeech.html\">ISCA Link</a>\
+		<br/>\
+		<br/>\
+		Arthur Hinsvark, Natalie Delworth, Miguel Del Rio, Quinten McNamara, Joshua Dong, Ryan Westerman, Michelle Huang, Joseph Palakapilly, Jennifer Drexler, Ilya Pirkin, Nishchal Bhandari, Miguel Jette. \"Accented Speech Recognition: A Survey\".\
+		<br/>\
+		<a class='links' href='https://arxiv.org/abs/2104.10747'>ArXiv Link</a>\
+		<br/>\
+		<br/>\
+		Matteo Alleman, Jonathan Mamou, Miguel A Del Rio, Hanlin Tang, Yoon Kim, SueYeon Chung. \"Syntactic Perturbations Reveal Representational Correlates of Hierarchical Phrase Structure in Pretrained Language Models\". In Proceedings of the 6th Workshop on Representation Learning for NLP (RepL4NLP-2021).\
+		<br/>\
+		<a class='links' href='https://arxiv.org/abs/2104.07578'>ArXiv Link</a> - <a class='links' href='https://aclanthology.org/2021.repl4nlp-1.27.pdf'>ACL Link</a>\
+		<br/>\
+		<br/>\
+		<span style='text-align:center; font-weight:bold; font-size:1.5em;'>2020</span>\
+		<br/>\
+		Jonathan Mamou, Hang Le, Miguel Del Rio, Cory Stephenson, Hanlin Tang, Yoon Kim, and SueYeon Chung. \"Emergence of separable manifolds in deep language representations\". In 37<sup>th</sup> International Conference on Machine Learning, ICML 2020. International Machine LearningSociety (IMLS), 2020.\
+		<br/>\
+		<a class='links' href='https://arxiv.org/abs/2006.01095'>ArXiv Link</a>\
+		<br/>\
+		<br/>\
+		Del R&iacute;o Fern&aacute;ndez, M. <em>Structure and Geometry in Sequence-Processing Neural Networks</em>. M. Eng. thesis. Massachusetts Institute of Technology, Department of Electrical Engineering and Computer Science, February, 2020.\
+		<br/>\
+		<a href=\"https://dspace.mit.edu/handle/1721.1/129881\">MIT DSpace Link</a>\
+		"
 	},
-	'Projects':{
+	'PersonalProjects':{
 		'color':'#e74c3c',
-		'title':"Projects",
+		'title':"Personal Projects",
 		'subtitle':"Check out some of the cool projects I've worked on over the years!",
 		'main':"\
-		<span style='text-align:center; font-weight:bold; font-size:1.25em;'>Publications</span>\
-		<br/>\
-		<a class='links' href='https://github.com/revdotcom/speech-datasets/tree/main/earnings21'>Earnings 21: A Practical Benchmark for ASR in the Wild</a> - as the paper's first author, I led the effort to gather, transcribe, and annotate the dataset. The paper describing our process to create and evaluate the dataset was accepted to Interspeech 2021 and can be found <a class='links' href='https://arxiv.org/abs/2104.11348'>here</a>.\
-		<br/>\
+		<span style='text-align:center; font-weight:bold; font-size:1.5em;'>Publications</span>\
 		<br/>\
 		<a class='links' href='https://github.com/schung039/contextual-repr-manifolds'>Emergence of Separable Manifolds in Deep Language Representations</a> - as one of the authors of this paper, I helped developed the code used to measure the emergence of linguistic information manifolds. The paper that resulted from these analyses was accepted to ICML 2020 and can be found <a class='links' href='https://arxiv.org/pdf/2006.01095.pdf'>here</a>.\
 		<br/>\
 		<br/>\
-		<span style='text-align:center; font-weight:bold; font-size:1.25em;'>Websites</span>\
+		<span style='text-align:center; font-weight:bold; font-size:1.5em;'>Websites</span>\
 		<br/>\
 		<a class='links' href='https://pique0822.github.io/VideoLectures/'>vid&middot;in</a> - this project was developed with the purpose of providing teachers an easy way to upload lectures online, ask questions, and receive student answers through the same website. This project included a feature that snapped a picture of the student as a question was answered to ensure that the appropriate person was answering the questions.\
 		<br/>\
@@ -68,9 +86,35 @@ var pages = {
 		<br/>\
 		<br/>\
 		"
+	},
+	'WorkProjects':{
+		'color':'#f39c12',
+		'title':"Work Projects",
+		'subtitle':"Featuring some of the work I've done so far in my career.",
+		'main':"\
+		<a href=\"https://www.rev.com\"><span style='text-align:center; font-weight:bold; font-size:1.5em;'>Rev.com</span></a>\
+		<br/>\
+		<em><b>Publications</b></em>\
+		<br/>\
+		<a class='links' href='https://github.com/revdotcom/speech-datasets/tree/main/earnings22'>Earnings-22: A Practical Benchmark for Accents in the Wild</a> - as the paper's first author, I led the effort to gather, transcribe, and demonstrate accent bias in Rev.com's production ASR model. The paper describing our process to create the dataset and perform our bias study has been submitted to Interspeech 2022 and can be found <a class='links' href='https://arxiv.org/abs/2203.15591'>here</a>.\
+		<br/>\
+		<br/>\
+		<a class='links' href='https://github.com/revdotcom/speech-datasets/tree/main/earnings21'>Earnings 21: A Practical Benchmark for ASR in the Wild</a> - as the paper's first author, I led the effort to gather, transcribe, and annotate the dataset. The paper describing our process to create and evaluate the dataset was accepted to Interspeech 2021 and can be found <a class='links' href='https://www.isca-speech.org/archive/interspeech_2021/delrio21_interspeech.html'>here</a>.\
+		<br/>\
+		<br/>\
+		<em><b>Automatic Spoken Language Identification</b></em>\
+		<br/>\
+		This API works by taking in a media file and predicting the most likely language spoken throughout it. For this project, I led the development of the underlying Docker container that runs the prediction. \
+		As owner, I was tasked with managing stakeholders, developing best practices for the project, writing tons of scripts, working with the AWS stack (such as ECR and S3), and post-deployment maintainance. \
+		<br/>\
+		<a href=\"https://docs.rev.ai/api/language-identification/get-started/\">API Documentation</a>\
+		<br/>\
+		"
 	}
 };
 
+const SELECTED_PAGE = 'selectedPage';
+const DEFAULT_PAGE = 'Home';
 
 $(document).ready(function() {
 	var cw = $('#profile-picture').width();
@@ -80,8 +124,13 @@ $(document).ready(function() {
 });
 
 function updatePage(){
-	vardef = pages[$('.selected').text()];
+	selectedName = localStorage.getItem(SELECTED_PAGE);
+	if (selectedName == null) {
+		selectedName = DEFAULT_PAGE;
+	}
+	vardef = pages[selectedName];
 
+	$('#' + selectedName).addClass('selected');
 	// Title
 	if("title" in vardef){
 		$('.content-title').html(vardef['title']);
@@ -111,16 +160,18 @@ function updatePage(){
 		$('.content-title').css('color',vardef['color']);
 		$('#profile-picture').css('box-shadow','0px 0px 5px 5px '+vardef['color']);
 		$('a').css('color', vardef['color']);
+		$('#at').css('color', vardef['color']);
+		$('#myName').css('color', vardef['color']);
 	}
 }
 
 function selectThis(name){
 	if($('.selected').text() != name)
 	{
-		vardef = pages[name];
 		$('.selected').css('color','white');
 		$('.selected').removeClass('selected');
-		$('#'+name).addClass('selected');
+
+		localStorage.setItem(SELECTED_PAGE, name);
 		updatePage();
 	}
 }
